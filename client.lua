fe2f73d2a76245d48e6b2cce982a946881879d93
Civic <76896192+CivicX@users.noreply.github.com>
2022-04-26 14:46:07 -0700

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,64 @@
+local displaytag = false
+local civicTags, maxDistance = {}, 10.0
+
+displayTags = function()
+  local playerPed = PlayerPedId()
+  local playerCoords = GetEntityCoords(playerPed)
+
+  for _, i in ipairs(GetActivePlayers()) do
+    local targetPed =  GetPlayerPed(i)
+    local targetCoords = GetEntityCoords(targetPed)
+    local serverid = GetPlayerServerId(i)
+    local nametags = Player(serverid).state.name
+
+    if not civicTags[i] or not IsMpGamerTagActive(civicTags[i].tag) then
+      civicTags[i] = {
+        tag = CreateFakeMpGamerTag(targetPed, nametags, false, false, 0),
+        ped = targetPed,
+      }
+    end
+
+    local nametag = civicTags[i].tag
+    local distance = #(targetCoords - playerCoords)
+
+    if (distance <= maxDistance) and HasEntityClearLosToEntity(playerPed, targetPed, 17) then
+      SetMpGamerTagVisibility(nametag, 0, true)
+    else
+      SetMpGamerTagVisibility(nametag, 0, false)
+    end
+  end
+end
+
+cleanupTags = function()
+  for _, v in pairs(civicTags) do
+      if IsMpGamerTagActive(v.tag) then
+          RemoveMpGamerTag(v.tag)
+      end
+  end
+  civicTags = {}
+end
+
+CreateThread(function()
+  local sleep = 50
+  while true do
+    if displaytag then
+      displayTags()
+      sleep = 50
+    else
+      sleep = 100
+    end
+    Wait(sleep)
+  end
+end)
+
+RegisterCommand('distag2', function()
+    displaytag = true
+end, false)
+
+RegisterCommand('distagoff', function()
+    displaytag = false
+    cleanupTags()
+end, false)
+
+RegisterKeyMapping("distag2", "Display Player Name Tags.", "keyboard", "h")
+RegisterKeyMapping("distagoff", "Disable Player Name Tags.", "keyboard", "left-alt") --You can change these bind in your FiveM Settings.
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Civic'
+
+description 'Player Tags.'
+
+lua54 'yes'
+
+client_scripts {
+    'client.lua',
+}
